02ae8ab6bbcd3d0bd587da511ec407c81b74d386
Daniel Costa <danielcc.jp@gmail.com>
2021-06-21 23:32:28 -0300

    feat: tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,18 @@ import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center">
       <Head>
         <title>Next Boilerplate</title>
         <meta
           name="description"
-          content="Next App boilerplate, typescript + eslint + prettier + editorconfig"
+          content="Next + Tailwind App boilerplate, typescript + eslint + prettier + editorconfig"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className="text-blue-900 text-8xl">
+        ✨ Next + Tailwind Boilerplate ✨
+      </h1>
     </div>
   );
 }
