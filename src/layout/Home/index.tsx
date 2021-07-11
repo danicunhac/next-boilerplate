248006b14f8903711fb8f988fda6b66e0723f320
Daniel Costa <danielcc.jp@gmail.com>
2021-07-11 18:51:43 -0300

    refactor: move head to home page

--- src/layout/Home/index.tsx
+++ src/layout/Home/index.tsx
@@ -1,5 +1,18 @@
 import { Title } from '@/common';
+import Head from 'next/head';
 
 export const Home = () => {
-  return <Title text="Next Boilerplate with Architecture" />;
+  return (
+    <>
+      <Head>
+        <title>Next Boilerplate</title>
+        <meta
+          name="description"
+          content="Next App boilerplate, typescript + eslint + prettier + editorconfig + jest + styled-components"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Title text="Next Boilerplate with Architecture" />
+    </>
+  );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,5 @@
 import { Home } from '@/layout/Home';
-import Head from 'next/head';
 
-const HomePage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Next Boilerplate</title>
-        <meta
-          name="description"
-          content="Next App boilerplate, typescript + eslint + prettier + editorconfig + jest + styled-components"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Home />
-    </div>
-  );
-};
+const HomePage = () => <Home />;
 
 export default HomePage;
