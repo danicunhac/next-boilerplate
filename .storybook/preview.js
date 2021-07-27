9b0487bbf8c7154bd16eda584fd1abcb9fb41619
Daniel Cunha <danielcc.jp@gmail.com>
2021-07-27 14:37:58 -0300

    feat: storybook config

---
+++ .storybook/preview.js
@@ -0,0 +1,20 @@
+import { GlobalStyle } from '../src/styles/global';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+export const decorators = [
+  Story => (
+    <>
+      <Story />
+      <GlobalStyle />
+    </>
+  ),
+];
